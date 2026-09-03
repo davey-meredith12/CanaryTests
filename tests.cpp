c8a3b2a276824c2798b46063b81f84812d14fdb7
davey-meredith12 <143116633+davey-meredith12@users.noreply.github.com>
2026-09-02 21:06:12 -0400

    Test printing has more information with TestInfo

--- tests.cpp
+++ tests.cpp
@@ -8,12 +8,17 @@ using std::string;
 using std::ostringstream;
 using std::vector;
 
-vector<void(*)()> tests;
+struct TestInfo{
+    string testName;
+    void (*testFunction)();
+};
+
+vector<TestInfo> tests;
 bool currentTestPassed = true;
 
 struct TestRegistration{
-    TestRegistration(void (*test)()){
-        tests.push_back(test);
+    TestRegistration(string name, void (*test)()){
+        tests.push_back({name, test});
     }
 };
 
@@ -24,9 +29,11 @@ void reportFailure(string file, int line, string message = ""){
 
 #define MAKE_NAME(a, b) a ## b
 
+#define STRINGIFY(x) #x
+
 #define TEST(name) \
     void name(); \
-    TestRegistration MAKE_NAME(registration, name)(name); \
+    TestRegistration MAKE_NAME(registration, name)(STRINGIFY(name), name); \
     void name()
 
 #define EXPECT_TRUE(condition) \
@@ -51,17 +58,37 @@ void reportFailure(string file, int line, string message = ""){
         } \
     } while (false);
 
-#define RUN_ALL_TESTS() \
-    for(auto test: tests) { \
-        currentTestPassed = true; \
-        test(); \
-        if (currentTestPassed){ \
-            cout << "Test Passed" << endl; \
-        } \
-        else { \
-            cout << "Test Failed" << endl; \
-        } \
+int RUN_ALL_TESTS(){
+    bool allTestsPassed = true;
+    int testsPassed = 0;
+    int testsFailed = 0;
+
+    for(auto test: tests) {
+        currentTestPassed = true;
+        test.testFunction();
+        if(currentTestPassed){
+            cout << test.testName << ": Test Passed" << endl;
+            testsPassed++;
+        }
+        else{
+            cout << test.testName << ": Test Failed" << endl;
+            allTestsPassed = false;
+            testsFailed++;
+        }
     }
+    cout << testsPassed + testsFailed << " tests ran." << endl;
+    cout << testsPassed << " tests passed." << endl;
+    cout << testsFailed << " tests failed." << endl;
+    return allTestsPassed ? 0 : 1;
+}
+
+
+
+
+
+
+
+
 
 TEST(FailTest){
     EXPECT_TRUE(false);
@@ -81,5 +108,5 @@ TEST(PassCompareTest){
 
 
 int main(){
-    RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
